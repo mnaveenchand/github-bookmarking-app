1a31419919c8d2fb6f4989b4a4069f2ca0b04976
Naveen Chand Madishetti <chand.naveen123@gmail.com>
2019-11-18 21:09:26 +0100

    added comments

--- routes/repositories.route.js
+++ routes/repositories.route.js
@@ -1,14 +1,14 @@
 const router = require('express').Router();
-const axios = require('axios')
+const axios = require('axios');
 
-
+//
 router.get('/search',async(request, response)=> {
     try {
         let searchstring =request.params.search;
         let url_api= ` https://api.github.com/search/repositories?q=${searchstring}`
-        const git_response = await axios.get(url_api,{headers: { 'content-type': 'application/json' }});
-        console.log("list repos: ",git_response.data.items[0]);
-        response.json(git_response.data.items[0])
+        const git_response = await axios.get(url_api,{headers: {'content-type': 'application/json'}});
+        console.log("list repos: ",git_response.data.items);
+        response.json(git_response.data.items)
     } catch (error) {
         console.error(error);
     }
