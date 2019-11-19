1263a0e2f295f8b5f1235a4c4f2c7bc4fccb44e0
Naveen Chand Madishetti <chand.naveen123@gmail.com>
2019-11-19 14:35:36 +0100

    code refactoring

--- routes/repositories.route.js
+++ routes/repositories.route.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 //
 router.get('/search',async(request, response)=> {
     try {
-        let searchstring =request.params.search;
+        let searchstring =request.params.searchterm;
         let url_api= ` https://api.github.com/search/repositories?q=${searchstring}`
         const git_response = await axios.get(url_api,{headers: {'content-type': 'application/json'}});
         console.log("list repos: ",git_response.data.items);
