d0f6fa9916c5c2a254166e249c2b46b680ca39f3
Naveen Chand Madishetti <chand.naveen123@gmail.com>
2019-11-30 00:55:37 +0100

    changes made

--- basic_auth/auth.js
+++ basic_auth/auth.js
@@ -6,21 +6,26 @@ router.post('/users', async (req, res) => {
     try {
       const hashedPassword = await bcrypt.hash(req.body.password, 10)
       const user = { username: req.body.username, password: hashedPassword }
+      const userstatus = users.find(user => user.username === req.body.username)
+      if (userstatus != null) {
+        return res.send('username exists, enter unique username')
+      } 
       users.push(user)
-      res.status(201).send("Success")
+      res.status(201).send(`${req.body.username} registration successfull`)
+
     } catch {
       res.status(500).send("failed to create user")
     }
   })
 
-  router.post('/users/login', async (req, res) => {
+  router.post('/login', async (req, res) => {
     const user = users.find(user => user.username === req.body.username)
     if (user == null) {
       return res.status(400).send('Cannot find user')
     }
     try {
       if(await bcrypt.compare(req.body.password, user.password)) {
-        res.send('Success')
+        res.send('login Successfull')
       } else {
         res.send('Enter valid password')
       }
